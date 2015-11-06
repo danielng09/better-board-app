9b9191f9c6938ff37a1761399c8c34003250b564
Daniel Ng <dvng13@gmail.com>
2015-11-06 12:37:57 -0800

    create promise middleware

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -1,9 +1,25 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 
-// const finalCreateStore = applyMiddleware(promiseMiddleware)(createStore);
+var promiseMiddleware = function () {
+  return (next) => (action) => {
+    const { types, promise, ...rest } = action;
+    if (!promise) {
+      return next(action);
+    }
+    const [REQUEST, SUCCESS, FAILURE] = types;
+
+    next({ ...rest, type: REQUEST});
+    return promise.then(
+      (res) => next({ ...rest, res, type: SUCCESS }),
+      (err) => next({ ...rest, err, type: FAILURE })
+    );
+  };
+}
+
+const finalCreateStore = applyMiddleware(promiseMiddleware)(createStore);
 
 export default function configureStore(initialState) {
-  const store = createStore(reducer, initialState);
+  const store = finalCreateStore(reducer, initialState);
   return store;
 }
