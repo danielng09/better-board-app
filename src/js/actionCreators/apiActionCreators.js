e8f5819a38d9caffab8c2ba067d330a0b6a55d4f
Daniel Ng <dvng13@gmail.com>
2015-11-06 12:37:16 -0800

    start api action creators

--- src/js/actionCreators/apiActionCreators.js
+++ src/js/actionCreators/apiActionCreators.js
@@ -5,14 +5,14 @@ export function fetchJobPostings() {
   var promise = new Promise((resolve, reject) => {
     request(apiConstants.POSTINGS_INDEX_URL, function(error, response, body) {
       let postings = JSON.parse(body);
-      resolve({ postings: postings })
+      resolve({ postings })
     }, function(err) {
       reject(err);
     });
   });
 
   return {
-    type: ['GET_POSTINGS_REQUEST', 'GET_POSTINGS_SUCCESS', 'GET_POSTINGS_FAILURE'],
+    types: ['GET_POSTINGS_REQUEST', 'GET_POSTINGS_SUCCESS', 'GET_POSTINGS_FAILURE'],
     promise: promise
   }
 }
