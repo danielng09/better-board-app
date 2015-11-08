c6753f6bc51025ab5284afecb917ae1ab80d4078
Daniel Ng <dvng13@gmail.com>
2015-11-08 15:58:56 -0800

    optimize logo images

--- src/js/components/postingDetail.js
+++ src/js/components/postingDetail.js
@@ -26,28 +26,31 @@ export default class PostingDetail extends React.Component {
             <i className="fa fa-building-o" />&nbsp;&nbsp;
             {postDetail.company}
           </p>
-          <span className="btn btn-default remove-button-features source-logo">
-            <img className="source-image" src={imageUrlConstants[sourceName]} />
-          </span>
           <p className="detail-location">
             <i style={{color: '#DBDBDB'}} className="fa fa-map-marker" />&nbsp;&nbsp;
             {postDetail.location}
           </p>
-          <span className="detail-date">
-            <i className="fa fa-calendar"></i>&nbsp;&nbsp;
-            {postDetail.date_posted}
-          </span>
-          <span className="activity">
-            <i style={{color:'#DBDBDB'}} className="fa fa-clock-o" />&nbsp;&nbsp;
-            {'Posted '  + postDetail.time_ago + ' ago'}
-          </span>
-
+          <p>
+            <span className="detail-date">
+              <i className="fa fa-calendar"></i>&nbsp;&nbsp;
+              {postDetail.date_posted}
+            </span>
+            <span className="activity">
+              <i style={{color:'#DBDBDB'}} className="fa fa-clock-o" />&nbsp;&nbsp;
+              {'Posted '  + postDetail.time_ago + ' ago'}
+            </span>
+          </p>
+          <p>
+            <a className="btn btn-warning detail-url"
+               target="_blank"
+               href={postDetail.url}><i className="fa fa-link"></i>&nbsp;&nbsp;Original
+            </ a>
+            <span className="btn btn-default remove-button-features source-logo">
+              <img className="source-image" src={imageUrlConstants[sourceName]} />
+            </span>
+          </p>
           <p className="detail-description"
             dangerouslySetInnerHTML={{__html: postDetail.description }} />
-          <a className="btn btn-info detail-url"
-             target="_blank"
-             href={postDetail.url}><i className="fa fa-link"></i>&nbsp;&nbsp;Original
-          </ a>
         </div>
       </div>
     )
--- src/js/constants/imageUrlConstants.js
+++ src/js/constants/imageUrlConstants.js
@@ -1,5 +1,5 @@
 export default {
-  INDEED: 'http://res.cloudinary.com/ds6oys8ca/image/upload/v1446920073/Indeed-Logo-image_vgzkpl.png',
-  STACKOVERFLOW: 'http://res.cloudinary.com/ds6oys8ca/image/upload/v1446938885/stackoverflow-logo_blsqge.png',
-  GITHUB: 'http://res.cloudinary.com/ds6oys8ca/image/upload/v1446938943/GitHub_Logo_ziiss2.png'
+  INDEED: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1446920073/Indeed-Logo-image_vgzkpl.png',
+  STACKOVERFLOW: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1446938885/stackoverflow-logo_blsqge.png',
+  GITHUB: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1446938943/GitHub_Logo_ziiss2.png'
 };
