8f9e58844fe783c7fc6962a6149888c787196294
Daniel Ng <dvng13@gmail.com>
2015-11-10 22:37:40 -0800

    add craigslist url

--- src/js/constants/imageUrlConstants.js
+++ src/js/constants/imageUrlConstants.js
@@ -1,5 +1,6 @@
 export default {
   INDEED: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1446920073/Indeed-Logo-image_vgzkpl.png',
   STACKOVERFLOW: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1446938885/stackoverflow-logo_blsqge.png',
-  GITHUB: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1446938943/GitHub_Logo_ziiss2.png'
+  GITHUB: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1446938943/GitHub_Logo_ziiss2.png',
+  CRAIGSLIST: 'http://res.cloudinary.com/ds6oys8ca/image/upload/c_scale,h_100/v1447223720/craigslist_vksykz.png'
 };
